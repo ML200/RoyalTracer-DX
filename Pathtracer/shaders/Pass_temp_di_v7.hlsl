6817915324e71374cc83bad82ab0e3106af8e573
Malte Lanz <55878865+ML200@users.noreply.github.com>
2025-07-05 05:23:53 +0200

    GI

--- Pathtracer/shaders/Pass_temp_di_v7.hlsl
+++ Pathtracer/shaders/Pass_temp_di_v7.hlsl
@@ -67,7 +67,7 @@ cbuffer CameraParams : register(b0)
 //─────────────────────────────────────────────────────────────────────────────
 //  TEMPORAL  DI
 //─────────────────────────────────────────────────────────────────────────────
-[numthreads(16, 16, 1)]
+[numthreads(16, 8, 1)]
 void main(uint3 tid : SV_DispatchThreadID)
 {
     if (tid.x >= IMG_W || tid.y >= IMG_H) return;
