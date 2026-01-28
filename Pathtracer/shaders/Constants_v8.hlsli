846f26aba72b7b2a6a1be99e56065622ee962bf0
Malte Lanz <55878865+ML200@users.noreply.github.com>
2026-01-28 20:08:33 +0100

    huiiii

--- Pathtracer/shaders/Constants_v8.hlsli
+++ Pathtracer/shaders/Constants_v8.hlsli
@@ -25,16 +25,16 @@
 
 
 // ___ GI Reuse ___
-#define TEMP_MCAP_GI 240
+#define TEMP_MCAP_GI 24
 
-#define SPAT_MCAP_GI 4800
+#define SPAT_MCAP_GI 48
 #define SPAT_EXP_GI 1.0f
 #define SPAT_RAD_MAX_GI 32
 #define SPAT_RAD_MIN_GI 24
 
 #define SPAT_COUNT_MAX_GI 2
 #define SPAT_COUNT_MIN_GI 1
-#define SPAT_TRIS_GI 6
+#define SPAT_TRIS_GI 3
 
 #define SPAT_MIN_M_GI 5
 #define SPAT_BETA_GI 1.0f
--- Pathtracer/shaders/Pass_shading_v8.hlsl
+++ Pathtracer/shaders/Pass_shading_v8.hlsl
@@ -64,7 +64,7 @@ void main(uint3 DTid : SV_DispatchThreadID)
     for (uint i = 0; i < 4; ++i) {
         if (any(view[i] != prevView[i])) cameraChanged = true;
     }
-    static const float MAX_SAMPLES     = 100000.0;
+    static const float MAX_SAMPLES     = 1000000.0;
 
     float4 prev        = gPermanentData[DTid.xy];   // rgb = running avg, a = N
     float3 prevAvg     = prev.rgb;
@@ -89,30 +89,22 @@ void main(uint3 DTid : SV_DispatchThreadID)
     gPermanentData[DTid.xy] = float4(newAvg, newSamples);
 
     float3 sceneLinear = accumulation;
-    float3 outSRGB       = sRGBGammaCorrection(saturate(sceneLinear));
-    gOutput[uint3(DTid.xy, 0)] = float4((float3)GetPHat(outSRGB), 1.0f);
-    gScratchPing[uint3(DTid.xy, 7)].x = GetPHat(outSRGB);
+    float3 outSRGB     = sRGBGammaCorrection(saturate(sceneLinear));
 
-    outSRGB       = sRGBGammaCorrection(saturate(sceneLinear));
-    gOutput[uint3(DTid.xy, 10)] = float4((float3)GetPHat(outSRGB), 1.0f);
-    gScratchPing[uint3(DTid.xy, 7)].y = GetPHat(outSRGB);
+    gOutput[uint3(DTid.xy, 0)] = float4(outSRGB, 1.0f);
 
-    outSRGB       = sRGBGammaCorrection(saturate(gt));
-    gOutput[uint3(DTid.xy, 11)] = float4((float3)GetPHat(outSRGB), 1.0f);
-    gScratchPing[uint3(DTid.xy, 7)].z = GetPHat(outSRGB);
-
-    // Load sdata for other buffers
     float2 dims = float2(IMG_W, IMG_H);
     uint   pixelIdx  = MapPixelID(dims, DTid.xy);
     SampleData sdata = loadSampleData(g_sample_current, pixelIdx);
-
+    gScratchPing[uint3(DTid.xy, 7)].x = GetPHat(output_GI);
+    gScratchPing[uint3(DTid.xy, 7)].y = GetPHat(newAvg);
+    gScratchPing[uint3(DTid.xy, 7)].z = GetPHat(gt);
     gScratchPing[uint3(DTid.xy, 7)].w = GetPHat(sdata.localKd);
 
-    gOutput[uint3(DTid.xy, 13)] = float4((float3)GetPHat(sdata.localKd), 1.0f);
-    gOutput[uint3(DTid.xy, 14)] = float4((float3)sdata.localPr, 1.0f);
-    gOutput[uint3(DTid.xy, 15)] = float4((float3)length(sdata.x1 - mul(viewI, float4(0, 0, 0, 1)).xyz), 1.0f);
-    gScratchPing[uint3(DTid.xy, 8)] = float4(sdata.localPr, length(sdata.x1 - mul(viewI, float4(0, 0, 0, 1)).xyz), 0.0f, 0.0f);
+    gOutput[uint3(DTid.xy, 10)] = gScratchPing[uint3(DTid.xy, 7)].x;
+    gOutput[uint3(DTid.xy, 11)] = gScratchPing[uint3(DTid.xy, 7)].y;
 
-    gOutput[uint3(DTid.xy, 12)] = float4(sdata.n1_s, 1.0f);
-    gScratchPing[uint3(DTid.xy, 8)] = float4(sdata.n1_s, 0.0f);
+    float depthVal = length(sdata.x1 - mul(viewI, float4(0, 0, 0, 1)).xyz);
+    gScratchPing[uint3(DTid.xy, 8)] = float4(sdata.localPr, depthVal, 0.0f, 0.0f);
+    gScratchPing[uint3(DTid.xy, 9)] = float4(sdata.n1_s, 0.0f);
 }
