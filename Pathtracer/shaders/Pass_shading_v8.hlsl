05bc4a1cf02a7a6bb5f4d817f4837e35e2114295
Malte Lanz <55878865+ML200@users.noreply.github.com>
2026-02-09 03:58:26 +0100

    more ai shenanigans

--- Pathtracer/shaders/Pass_shading_v8.hlsl
+++ Pathtracer/shaders/Pass_shading_v8.hlsl
@@ -53,7 +53,7 @@ void main(uint3 DTid : SV_DispatchThreadID)
     if (DTid.x >= gImageWidth || DTid.y >= gImageHeight) return;
     gOutput[uint3(DTid.xy, 0)] = float4(0, 0, 0, 0);
 
-    float3 output_DI = 0.0f;//gScratchPing[uint3(DTid.xy, 1)];
+    float3 output_DI = gScratchPing[uint3(DTid.xy, 1)];
     float3 output_GI = gScratchPing[uint3(DTid.xy, 2)];
 
     float3 accumulation = output_DI + output_GI;
