3a9950f726ae3ecbac388e355f4d7c9c2669420a
Malte Lanz <55878865+ML200@users.noreply.github.com>
2026-04-04 03:07:26 +0200

    movement fix

--- Pathtracer/shaders/Pass_shading_v8.hlsl
+++ Pathtracer/shaders/Pass_shading_v8.hlsl
@@ -10,11 +10,11 @@ void main(uint3 DTid : SV_DispatchThreadID)
     if (DTid.x >= gImageWidth || DTid.y >= gImageHeight) return;
     gOutput[uint3(DTid.xy, 0)] = float4(0, 0, 0, 0);
 
-    float3 output_DI = gScratchPing[uint3(DTid.xy, 1)];
-    float3 output_GI = gScratchPing[uint3(DTid.xy, 2)];
+    float3 output_DI  = gScratchPing[uint3(DTid.xy, 1)];
+    float3 output_GI  = gScratchPing[uint3(DTid.xy, 2)];
+    float3 sunDirect  = gScratchPing[uint3(DTid.xy, 3)].rgb;
 
-    float3 accumulation = output_DI + output_GI;
-    //float3 gt = gScratchPing[uint3(DTid.xy, 3)];
+    float3 accumulation = output_DI + output_GI + sunDirect;
 
     bool cameraChanged = false;
     [unroll]
