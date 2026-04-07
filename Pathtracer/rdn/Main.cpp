39f50586eb42a8142a6b7ae53cc87ee795b953df
Malte Lanz <55878865+ML200@users.noreply.github.com>
2026-04-07 22:43:56 +0200

    yooo

--- Pathtracer/rdn/Main.cpp
+++ Pathtracer/rdn/Main.cpp
@@ -20,6 +20,7 @@ public:
     std::vector<MeshDefinition> GetMeshes() override {
         return {
             { "./tS2.glb", XMMatrixIdentity() },
+            { "./giga_sphere.obj", XMMatrixIdentity()*XMMatrixTranslation(0,2,0) },
             //{ "./car/car.obj", XMMatrixIdentity() }
         };
     }
