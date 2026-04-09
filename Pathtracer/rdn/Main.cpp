25cb346007d9d6347a7b5372b29df399db2295f5
Malte Lanz <55878865+ML200@users.noreply.github.com>
2026-04-09 15:30:14 +0200

    Update Main.cpp

--- Pathtracer/rdn/Main.cpp
+++ Pathtracer/rdn/Main.cpp
@@ -19,7 +19,7 @@ class BistroScene : public SceneDefinition {
 public:
     std::vector<MeshDefinition> GetMeshes() override {
         return {
-            { "./newportnews.glb", XMMatrixIdentity() },
+            { "./twr.glb", XMMatrixIdentity() },
             //{ "./giga_sphere.obj", XMMatrixIdentity()*XMMatrixTranslation(0,2,0) },
             //{ "./car/car.obj", XMMatrixIdentity() }
         };
